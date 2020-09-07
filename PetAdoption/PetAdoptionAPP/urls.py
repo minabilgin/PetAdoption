1dc1fd1a2921fedc22e471aafe57ff6c4cb3b83c
minabilgin <begum.bilgin@isik.edu.tr>
2020-09-07 21:05:00 +0300

    Login Logout
    Urls for login and logout

--- PetAdoption/PetAdoptionAPP/urls.py
+++ PetAdoption/PetAdoptionAPP/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
 
     path('', views.homePage, name='homePage'),
+    path('login/', views.login_request, name='login'),
+    path('logout/', views.logout_request, name="logout"),
 
 ]
--- PetAdoption/PetAdoptionAPP/views.py
+++ PetAdoption/PetAdoptionAPP/views.py
@@ -1,6 +1,33 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import redirect
+from django.contrib import messages
 
 # Create your views here.
 
+
 def homePage(request):
     return render(request, 'homePage.html', {})
+
+
+def login_request(request):
+    print("login req")
+    username = request.POST.get('username')
+    password = request.POST.get('password')
+    user = authenticate(request, username=username, password=password)
+    userid = user
+    if user is not None:
+        login(request, user)
+        print("login not null")
+        return redirect('/', {'logged_in'})
+    else:
+        messages.error(request, 'username or password not correct')
+        print("login null")
+        return redirect('/')
+
+
+def logout_request(request):
+    if request.method == "POST":
+        logout(request)
+    return redirect('homePage')
+
