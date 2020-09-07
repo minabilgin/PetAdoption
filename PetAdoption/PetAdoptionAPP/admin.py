1f7521c1c3c47bde29ab085a159d606645e10bf0
minabilgin <begum.bilgin@isik.edu.tr>
2020-09-07 15:59:44 +0300

    Admin & Models
    admin.py and models.py are updated

--- PetAdoption/PetAdoptionAPP/admin.py
+++ PetAdoption/PetAdoptionAPP/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Pet, PaidPets, PetStuff
 
 # Register your models here.
+
+
+@admin.register(Pet)
+class PetAdmin(admin.ModelAdmin):
+    list_display = ["id", "sex", "name", "age", "type"]
+
+
+@admin.register(PaidPets)
+class PaidPetAdmin(admin.ModelAdmin):
+    list_display = ["id", "sex", "name", "age", "type", "price"]
+
+
+@admin.register(PetStuff)
+class PetStuffAdmin(admin.ModelAdmin):
+    list_display = ["name", "price", "type", "image", "description", "location"]
+
+
--- PetAdoption/PetAdoptionAPP/models.py
+++ PetAdoption/PetAdoptionAPP/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Pet(models.Model):
+    id = models.IntegerField(max_length=10, null=False)
+    sex = models.CharField(max_length=10, null=False)
+    name = models.CharField(max_length=10)
+    age = models.IntegerField(max_length=3, null=False)
+    type = models.CharField(max_length=50, null=False)
+
+
+class PaidPets(models.Model):
+    price = models.IntegerField(max_length=10)
+    pets = models.OneToOneField(Pet, on_delete=models.CASCADE, null=True)
+
+
+class PetStuff(models.Model):
+    name = models.CharField(max_length=25, null=False)
+    price = models.IntegerField(max_length=10, null=False)
+    type = models.CharField(max_length=50, null=False)
+    image = models.ImageField(null=False)
+    description = models.CharField(max_length=200, null=False)
+    location = models.CharField(max_length=100, null=False)
+
+
+
+
+
